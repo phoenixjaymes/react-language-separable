0093babf4be00d641bcbf3c28d200251d0e7f6ec
Jaymes Young <jaymes@phoenixjaymes.com>
2019-10-21 13:20:54 -0700

    imported Porvider, createStore and PageReducer. Created stor and wrapped App in Provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PageReducer from './reducers/separable';
+
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = createStore(
+  PageReducer,
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
