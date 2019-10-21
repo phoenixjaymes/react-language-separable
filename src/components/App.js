c1793869488b8e35b3739aaf404829a6b2da257f
Jaymes Young <jaymes@phoenixjaymes.com>
2019-10-21 13:29:00 -0700

    imported components and added them to return

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import '../App.css';
 
+import ErrorBoundary from './ErrorBoundary';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+import separable from '../data/separable';
+
 function App() {
+  const { page, words } = separable;
+
   return (
-    <div>
-      Yes
+    <div className="main-container">
+      <Header />
+      <ErrorBoundary>
+        <Main
+          words={words}
+          page={page}
+        />
+      </ErrorBoundary>
+      <Footer />
     </div>
   );
 }
