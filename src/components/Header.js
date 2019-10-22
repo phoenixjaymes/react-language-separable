0e74501f2496f7118380a6d7ba75e653f753f001
Jaymes Young <jaymes@phoenixjaymes.com>
2019-10-22 14:52:18 -0700

    added change page prop and added it to buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Header = () => (
+const Header = ({ changePage }) => (
   <header>
     <div className="inner-container">
       <p>Separable Verbs</p>
       <nav>
         <ul>
-          <li><button type="button">Home</button></li>
-          <li><button type="button">German</button></li>
-          <li><button type="button">Dutch</button></li>
-          <li><button type="button">Afrikaans</button></li>
+          <li><button type="button" onClick={() => changePage('home')}>Home</button></li>
+          <li><button type="button" onClick={() => changePage('de')}>German</button></li>
+          <li><button type="button" onClick={() => changePage('nl')}>Dutch</button></li>
+          <li><button type="button" onClick={() => changePage('za')}>Afrikaans</button></li>
         </ul>
       </nav>
     </div>
