02f07315ed75b110bee95acb33a8f3d57c9d8965
Jaymes Young <jaymes@phoenixjaymes.com>
2019-10-27 13:52:08 -0700

    refactored code to include active link state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,37 @@
 import React from 'react';
 
-const Header = ({ changePage }) => (
-  <header>
-    <div className="inner-container">
-      <p>Separable Verbs</p>
-      <nav>
-        <ul>
-          <li><button type="button" onClick={() => changePage('home')}>Home</button></li>
-          <li><button type="button" onClick={() => changePage('de')}>German</button></li>
-          <li><button type="button" onClick={() => changePage('nl')}>Dutch</button></li>
-          <li><button type="button" onClick={() => changePage('za')}>Afrikaans</button></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-);
+const Header = ({ page, changePage }) => {
+  const pages = [
+    { id: 'home', name: 'Home' },
+    { id: 'de', name: 'German' },
+    { id: 'nl', name: 'Dutch' },
+    { id: 'za', name: 'Afrikaans' },
+  ];
+
+  const buttons = pages.map((pageItem) => (
+    <li>
+      <button
+        className={page === pageItem.id ? 'active' : ''}
+        type="button"
+        onClick={() => changePage(pageItem.id)}
+      >
+        {pageItem.name}
+      </button>
+    </li>
+  ));
+
+  return (
+    <header>
+      <div className="inner-container">
+        <p>Separable Verbs</p>
+        <nav>
+          <ul>
+            {buttons}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
