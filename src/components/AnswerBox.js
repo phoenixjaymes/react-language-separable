0b905f0aca3e74049f9e1ba37b83451f8e057945
Jaymes Young <jaymes@phoenixjaymes.com>
2019-10-27 13:26:35 -0700

    put answertText & answer in p tags with classes

--- src/components/AnswerBox.js
+++ src/components/AnswerBox.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const AnswerBox = ({ answerClass, answerText, answer}) => (
   <div className={`answer-box ${answerClass}`}>
-    {answerText}
-    <br />
-    {answer}
+    <div>
+      <p className="answer-box__text">{answerText}</p>
+      <p className="answer-box__answer">{answer}</p>
+    </div>
   </div>
 );
 
