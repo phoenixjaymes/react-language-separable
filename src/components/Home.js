28ebf7667a35d22329be946f373a4fa6c79bc48a
Jaymes Young <jaymes@phoenixjaymes.com>
2021-11-29 19:09:46 -0800

    Added change page prop
    Added change page prop.
    Added onClick to button.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = () => (
+const Home = ({ changePage }) => (
   <section>
     <h1 className="text-center">Separable Verbs</h1>
 
@@ -10,15 +10,18 @@ const Home = () => (
     </dl>
 
     <p>
-    In German, Dutch, and Afrikaans some verbs separate into two parts when used in present tense.
+      In German, Dutch, and Afrikaans some verbs separate into two parts when used in present tense.
     </p>
 
     <p>
       Here is your chance to test your knowledge of separable verbs.
+      You will be given 20 words to complete.
+      Any incorrect words will be added to the end of the list of words.
+      You will keep getting words until you answer all words correctly.
     </p>
 
     <div className="hero-buttons">
-      <button type="button">German Verbs</button>
+      <button type="button" onClick={() => changePage('de')}>German Verbs</button>
     </div>
   </section>
 );
